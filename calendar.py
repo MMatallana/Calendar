7658b1dd44c62e53bff6756d5fadfc0e6c9931de
MMatallana <40077810+MMatallana@users.noreply.github.com>
2018-06-20 00:34:46 -0500

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,6 +1,17 @@
-import calendar
+from tkinter import *
 import datetime
-yy=datetime.year()
-mm=datetime.month()
-
-print(calendar.month(yy, mm))
+import calendar
+yy=datetime.date.today().year
+mm=datetime.date.today().month
+root = Tk()
+text = Text(root)
+text.insert(INSERT, calendar.month(yy, mm))
+text.insert(END, " ")
+text.pack()
+text.tag_add("here", "1.0", "2.0")
+text.tag_add("start", "2.0", "3.0")
+text.tag_add("fart", "3.0", "8.0")
+text.tag_config("here", background = "red", foreground = "black")
+text.tag_config("start", background = "yellow", foreground = "black")
+text.tag_config("fart", background = "green", foreground = "black")
+root.mainloop()
