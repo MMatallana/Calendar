9c24f22e310f6f4571fc55c3ae28c6a20dd748e2
MMatallana <40077810+MMatallana@users.noreply.github.com>
2018-07-05 18:27:44 -0500

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,20 +1,30 @@
 import datetime
 l={}
+n={}
+p=0
+o=0
 data=open("datos.txt","r+")
-d=dd=datetime.date.today().day
+d=datetime.date.today().day
 m=datetime.date.today().month
 y=datetime.date.today().year
 for line in data:
-	p=line.rstrip(":")
-	o=line.lstrip(":")
-	print(p)
-	print(line)
-	print(line.rstrip(":"))
-l.update({p:o})
+	n.update({line})
+	if n=={}:
+		print()
+	else:
+		p=line.rstrip(":")
+		o=line.lstrip(":")
+		print(p)
+		print(line)
+		print(line.rstrip(":"))
+		l.update({p:o})
 print("El día de hoy es: " + str(d)+ "/" +str(m)+ "/" + str(y))
 n=int(input("Si desea ver los pendientes pulse 1 y si desea añadir una fecha pulse 2: "))
 if n==1:
- print(l)
+ if l=={}:
+	 print("No hay eventos")
+ else:
+ 	print(l)
 elif n==2:
 	i=str(input("Nombre: "))
 	j=str(input("Fecha: "))
