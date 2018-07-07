6840e3b5488802a6fb0b150abe801872ba3bb20b
alexia-torres <40325960+alexia-torres@users.noreply.github.com>
2018-07-07 11:47:44 -0500

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,3 +1,45 @@
+lista=[['201720026','Acevedo Esquivel, Daniel Gonzalo',[14,6,14]],
+['201720058','Castro Alejo, Carlos Piero',[0,0,7]],
+['201720060','Crispin Flores, Carlos Jhack',[20,20,7]],
+['201720061','Delgado Díaz, José Iván',[20,7,13]],
+['201720031','Díaz Gardini, Andrea Gabriela',[14,13,20]],
+['201720063','Ferruzo García, Sheryl Damarys',[6,10,7]],
+['201720068','López Reyna, Miriam',[6,10,7]],
+['201720072','Mendoza Baldarrago, Brayan Brus',[12,14,7]],
+['201720075','Meza Cruzado, Tania Lisbet',[12,8,14]],
+['201720076','Mory Reyes, Mayte Mirella',[6,15,7]],
+['201720038','Mungi Osores, Andree Cristhian',[20,14,14]],
+['201720039','Olabarrera Vilcatoma, José Manuel',[0,0,7]],
+['201720041','Paz Coronel, Walter Alejandro',[14,9,7]],
+['201720083','Polo Mendoza, Gabriela',[6,7,14]],
+['201620053','Ponce Contreras, Luis Eduardo',[20,20,20]],
+['201710641','Silva Calderón, Gonzalo Alejandro',[14,15,20]],
+['201720090','Sime Costilla, William',[2,0,7]],
+['201720107','Sánchez Paredes, Josué Andoni',[20,20,14]],
+['201720046','Sánchez Albarracín, Guillermo Fernando',[20,20,14]],
+['201720092','Tasayco Castilla, Marlyn Brenda',[12,4,0]],
+['201720048','Velazco Champac, Geraldyne Eliane',[6,5,0]]]
+
+def bubbleSort(lista):
+  for num in range(len(lista) - 1, 0,-2):
+    for j in range(num):
+      prom2=(lista[j+1][2][0]+lista[j+1][2][1]+lista[j+1][2][2])/3
+      prom1=(lista[j][2][0]+lista[j][2][1]+lista[j][2][2])/3
+      if prom1>prom2:
+        aux = lista[j]
+        lista[j] = lista[j + 1]
+        lista[j + 1] = aux
+  print("MENORES NOTAS")
+  print(lista[0][1])
+  print(lista[1][1])
+  print(lista[2][1])
+  print("MAYORES NOTAS:")
+  print(lista[20][1])
+  print(lista[19][1])
+  print(lista[18][1])
+
+bubbleSort(lista)
+
 import datetime
 d=datetime.date.today().day
 m=datetime.date.today().month
